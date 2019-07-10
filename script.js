899daa72be2d51230e1c6dd80c40f515b3eba9be
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-10 16:24:06 +0200

    Effettuata prima parte esercizio.

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
-// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. 
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
 // Stampare a schermo attraverso il for in tutte le proprietà.
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
 // - Dare la possibilità all’utente attraverso 3 prompt di
 // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+
+var elemento = document.getElementById('mio_id');
+
+var studenti = {
+  "nome": "Giuseppe",
+  "cognome": "Rossi",
+  "età": 17
+};
+
+var text = "";
+
+for (var x in studenti) {
+  text += studenti[x] + " ";
+  elemento.innerHTML ="Alunno: " + text;
+}
+
+console.log(studenti);
