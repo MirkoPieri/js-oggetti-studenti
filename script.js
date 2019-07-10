773fc0f4494703575f49b3add227ec7c6c9615b1
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-10 17:12:06 +0200

    parte 3 dell'esercizio.

--- script.js
+++ script.js
@@ -5,8 +5,14 @@
 // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
 
 var elemento = document.getElementById('mio_id');
+var elemento1 = document.getElementById('mio_id1');
 
-var studenti = {
+var si = s, no = n;
+
+si.toUpperCase();
+si.toUpperCase();
+
+var studente1 = {
   "nome": "Giuseppe",
   "cognome": "Rossi",
   "età": 17
@@ -14,9 +20,51 @@ var studenti = {
 
 var text = "";
 
-for (var x in studenti) {
-  text += studenti[x] + " ";
+for (var x in studente1) {
+  text += studente1[x] + " ";
   elemento.innerHTML ="Alunno: " + text;
 }
 
+
+var studente2 = {
+  "nome": "Mario",
+  "cognome": "Bianchi",
+  "età": 18
+};
+
+var studente3 = {
+  "nome": "Luigi",
+  "cognome": "Mattarella",
+  "età": 17
+};
+
+var studente4 = {
+  "nome": "Francesca",
+  "cognome": "Rossi",
+  "età": 17
+};
+
+var studente5 = {
+  "nome": "Maria",
+  "cognome": "Verdi",
+  "età": 20
+};
+
+var studenti = [studente2, studente3, studente4, studente5];
 console.log(studenti);
+
+var text2 = "";
+for (var i = 0; i < studenti.length; i++) {
+  text2 += "<li>" + studenti[i].nome + " " + studenti[i].cognome + "</li>";
+  elemento1.innerHTML = text2;
+}
+
+
+inserimento = prompt("Vuoi inserire un nuovo utente? s/n")
+inserimento.toUpperCase();
+
+if (inserimento == si) {
+  var nome = prompt("Inserisci il nome dello studente");
+  var cognome = prompt("Inserisci il cognome dello studente");
+  var eta = prompt("Inserisci l'età dello studente");
+}
